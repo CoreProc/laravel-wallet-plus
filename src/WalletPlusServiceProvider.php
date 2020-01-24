093f75a06e5bf954fccf97d00422475688e3e5a8
Chris Bautista <chris.bautista@coreproc.ph>
2020-01-25 01:33:42 +0800

    Migration file can only be published once

--- src/WalletPlusServiceProvider.php
+++ src/WalletPlusServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace CoreProc\WalletPlus;
 
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 
 class WalletPlusServiceProvider extends ServiceProvider
@@ -9,14 +11,12 @@ class WalletPlusServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application services.
      */
-    public function boot()
+    public function boot(Filesystem $filesystem)
     {
-        if (! class_exists('CreateWalletPlusTables')) {
-            $this->publishes([
-                __DIR__ . '/../database/migrations/create_wallet_plus_tables.php.stub' => database_path('migrations/' .
-                    date('Y_m_d_His', time()) . '_create_wallet_plus_tables.php'),
-            ], 'migrations');
-        }
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_wallet_plus_tables.php.stub' =>
+                $this->getMigrationFileName($filesystem),
+        ], 'migrations');
     }
 
     /**
@@ -25,5 +25,16 @@ class WalletPlusServiceProvider extends ServiceProvider
     public function register()
     {
         // $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'wallet-plus');
+    }
+
+    protected function getMigrationFileName(Filesystem $filesystem)
+    {
+        $timestamp = date('Y_m_d_His');
+
+        return Collection::make($this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem) {
+                return $filesystem->glob($path . '*_create_wallet_plus_tables.php');
+            })->push($this->app->databasePath() . "/migrations/{$timestamp}_create_wallet_plus_tables.php")
+            ->first();
     }
 }
